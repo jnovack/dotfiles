b5e4f288da8e49e33494c1d62752e254648dfe8a
Justin J. Novack <jnovack@gmail.com>
2012-05-31 16:46:56 -0400

    install_vim.sh - Corrected directories for install

--- install_vim.sh
+++ install_vim.sh
@@ -2,8 +2,8 @@
 # MacVim - http://code.google.com/p/macvim/
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.vim/
-ln -s $DIR/vim/colors colors
-ln -s $DIR/vim/doc doc
-ln -s $DIR/vim/plugin plugin
-ln -s $DIR/vim/syntax syntax
+ln -s $DIR/vim/colors ~/.vim/colors
+ln -s $DIR/vim/doc ~/.vim/doc
+ln -s $DIR/vim/plugin ~/.vim/plugin
+ln -s $DIR/vim/syntax ~/.vim/syntax
 ln -s $DIR/vim/_vimrc ~/.vimrc
