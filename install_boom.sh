c7c9d77c2373f4518eba652366ba50858c999253
Justin J. Novack <jnovack@gmail.com>
2012-10-12 16:37:46 -0400

    Added Ruby from RVM

--- install_boom.sh
+++ install_boom.sh
@@ -3,9 +3,8 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 read -p "Press [ENTER] when Xcode has been installed from the App Store (or break now to cancel.)"
 read -p "Press [ENTER] when MacPorts has been installed from www.macports.org (or break now to cancel.)"
-sudo port selfupdate
-sudo port upgrade outdated
-sudo port install ruby
+curl -L https://get.rvm.io | bash -s stable --ruby
+source ~/.rvm/scripts/rvm
 sudo gem install boom
 ln -s $DIR/boom/boom.conf ~/.boom.conf
 ln -s $DIR/boom/boom ~/.boom
