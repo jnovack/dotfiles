fb7542e683ab731bc5f9b273dad6135e23bbee00
Justin J. Novack <jnovack@gmail.com>
2012-11-15 20:20:40 -0500

    updated installation script

--- install_git.sh
+++ install_git.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-git config --global alias.sum "log --graph --decorate --pretty=oneline --color --abbrev-commit --all"
-git config --global alias.history "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short --color --all"
-
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cat $DIR/_gitconfig >> ~/.gitconfig
