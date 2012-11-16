7896be03bc353f596d6456cc82b5d5455288fa0c
Justin J. Novack <jnovack@gmail.com>
2012-11-15 20:08:30 -0500

    update boom to use rvm.io

--- install_boom.sh
+++ install_boom.sh
@@ -2,8 +2,8 @@
 # boom - http://zachholman.com/boom/
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 read -p "Press [ENTER] when Xcode has been installed from the App Store (or break now to cancel.)"
-read -p "Press [ENTER] when MacPorts has been installed from www.macports.org (or break now to cancel.)"
-curl -L https://get.rvm.io | bash -s stable --ruby
+read -p "Press [ENTER] when Command Line Tools has been installed from within Xcode (or break now to cancel.)"
+curl -L https://get.rvm.io | bash -s stable --rails
 source ~/.rvm/scripts/rvm
 sudo gem install boom
 ln -s $DIR/boom/boom.conf ~/.boom.conf
