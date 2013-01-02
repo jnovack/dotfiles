9909ba419150fec107c6e051c6397c0c095979ad
Justin J. Novack <jnovack@parxcasino.com>
2013-01-02 11:09:27 -0500

    Carefully corrected color codes

--- bash/colorize.sh
+++ bash/colorize.sh
@@ -7,45 +7,45 @@
 # Purpose: Changes Root Prompt to be Red
 # Documentation: Copy this file to /etc/profile.d/ and chmod +x
 
-txtblk='\033[0;30m' # Black - Regular
-txtred='\033[0;31m' # Red
-txtgrn='\033[0;32m' # Green
-txtylw='\033[0;33m' # Yellow
-txtblu='\033[0;34m' # Blue
-txtpur='\033[0;35m' # Purple
-txtcyn='\033[0;36m' # Cyan
-txtwht='\033[0;37m' # White
-bldblk='\033[1;30m' # Black - Bold
-bldred='\033[1;31m' # Red
-bldgrn='\033[1;32m' # Green
-bldylw='\033[1;33m' # Yellow
-bldblu='\033[1;34m' # Blue
-bldpur='\033[1;35m' # Purple
-bldcyn='\033[1;36m' # Cyan
-bldwht='\033[1;37m' # White
-undblk='\033[4;30m' # Black - Underline
-undred='\033[4;31m' # Red
-undgrn='\033[4;32m' # Green
-undylw='\033[4;33m' # Yellow
-undblu='\033[4;34m' # Blue
-undpur='\033[4;35m' # Purple
-undcyn='\033[4;36m' # Cyan
-undwht='\033[4;37m' # White
-bakblk='\033[40m'   # Black - Background
-bakred='\033[41m'   # Red
-bakgrn='\033[42m'   # Green
-bakylw='\033[43m'   # Yellow
-bakblu='\033[44m'   # Blue
-bakpur='\033[45m'   # Purple
-bakcyn='\033[46m'   # Cyan
-bakwht='\033[47m'   # White
-txtrst='\033[m'   	# Text Reset
+txtblk='\[\e[0;30m\]' # Black - Regular
+txtred='\[\e[0;31m\]' # Red
+txtgrn='\[\e[0;32m\]' # Green
+txtylw='\[\e[0;33m\]' # Yellow
+txtblu='\[\e[0;34m\]' # Blue
+txtpur='\[\e[0;35m\]' # Purple
+txtcyn='\[\e[0;36m\]' # Cyan
+txtwht='\[\e[0;37m\]' # White
+bldblk='\[\e[1;30m\]' # Black - Bold
+bldred='\[\e[1;31m\]' # Red
+bldgrn='\[\e[1;32m\]' # Green
+bldylw='\[\e[1;33m\]' # Yellow
+bldblu='\[\e[1;34m\]' # Blue
+bldpur='\[\e[1;35m\]' # Purple
+bldcyn='\[\e[1;36m\]' # Cyan
+bldwht='\[\e[1;37m\]' # White
+undblk='\[\e[4;30m\]' # Black - Underline
+undred='\[\e[4;31m\]' # Red
+undgrn='\[\e[4;32m\]' # Green
+undylw='\[\e[4;33m\]' # Yellow
+undblu='\[\e[4;34m\]' # Blue
+undpur='\[\e[4;35m\]' # Purple
+undcyn='\[\e[4;36m\]' # Cyan
+undwht='\[\e[4;37m\]' # White
+bakblk='\[\e[40m\]'   # Black - Background
+bakred='\[\e[41m\]'   # Red
+bakgrn='\[\e[42m\]'   # Green
+bakylw='\[\e[43m\]'   # Yellow
+bakblu='\[\e[44m\]'   # Blue
+bakpur='\[\e[45m\]'   # Purple
+bakcyn='\[\e[46m\]'   # Cyan
+bakwht='\[\e[47m\]'   # White
+txtrst='\[\e[m\]'   	# Text Reset
 
 # Set Prompt
 # \w = Full Path           eg.  /usr/www/html
 # \W = Current directory   eg.  html
 if [ `id -u` = 0 ]; then
-	PS1="\[$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldred[$txtred\u@$bldred\h $txtgrn\w$bldred]$txtrst\\$ \]"
+	PS1="$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldred[$txtred\u@$bldred\h $txtgrn\w$bldred]$txtrst\\$ "
 else
-	PS1="\[$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldblu[$txtblu\u@$bldblu\h $txtgrn\w$bldblu]$txtrst\\$ \]"
+	PS1="$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldblu[$txtblu\u@$bldblu\h $txtgrn\w$bldblu]$txtrst\\$ "
 fi
