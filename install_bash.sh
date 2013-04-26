be3b6b52f970e60cb78a0071396005766605efc4
Justin J. Novack <jnovack@gmail.com>
2013-04-26 09:12:28 -0400

    Added new computer installer

---
+++ install_bash.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir ~/bin/
+ln -s $DIR/bash/ssh ~/bin/
+ln -s $DIR/bash/ssh-copy-id /usr/local/bin/
+
+cat $DIR/bash/profile >> ~/.profile
+echo "export PROMPT_COMMAND='sh $DIR/bash/gitprompt.sh'" >> ~/.profile
+echo "source $DIR/bash/colorize.sh" >> ~/.profile
+cat $DIR/bash/complete.sh >> ~/.profile
+cat $DIR/bash/aliases >> ~/.profile
