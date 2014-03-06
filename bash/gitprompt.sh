6c35151e69b0730a718d72a32985b58d36a4c86b
Justin J. Novack <jnovack@parxcasino.com>
2014-03-06 10:28:05 -0500

    learned the wonders of tput

--- bash/gitprompt.sh
+++ bash/gitprompt.sh
@@ -10,40 +10,59 @@
 # Purpose: Prepends the following line above the command prompt.
 # GIT Status: 	Added:        0 | Updated:        0 | Deleted:        0
 
-txtblk='\033[0;30m' # Black - Regular
-txtred='\033[0;31m' # Red
-txtgrn='\033[0;32m' # Green
-txtylw='\033[0;33m' # Yellow
-txtblu='\033[0;34m' # Blue
-txtpur='\033[0;35m' # Purple
-txtcyn='\033[0;36m' # Cyan
-txtwht='\033[0;37m' # White
-bldblk='\033[1;30m' # Black - Bold
-bldred='\033[1;31m' # Red
-bldgrn='\033[1;32m' # Green
-bldylw='\033[1;33m' # Yellow
-bldblu='\033[1;34m' # Blue
-bldpur='\033[1;35m' # Purple
-bldcyn='\033[1;36m' # Cyan
-bldwht='\033[1;37m' # White
-undblk='\033[4;30m' # Black - Underline
-undred='\033[4;31m' # Red
-undgrn='\033[4;32m' # Green
-undylw='\033[4;33m' # Yellow
-undblu='\033[4;34m' # Blue
-undpur='\033[4;35m' # Purple
-undcyn='\033[4;36m' # Cyan
-undwht='\033[4;37m' # White
-bakblk='\033[40m'   # Black - Background
-bakred='\033[41m'   # Red
-bakgrn='\033[42m'   # Green
-bakylw='\033[43m'   # Yellow
-bakblu='\033[44m'   # Blue
-bakpur='\033[45m'   # Purple
-bakcyn='\033[46m'   # Cyan
-bakwht='\033[47m'   # White
-txtrst='\033[m'   	# Text Reset
+reset=$(   tput sgr0 )
+bold=$(    tput bold )
+under=$(   tput smul )
 
+# Unstyled colors
+black=$(   tput setaf 0 || tput AF 0 )
+red=$(     tput setaf 1 || tput AF 1 )
+green=$(   tput setaf 2 || tput AF 2 )
+yellow=$(  tput setaf 3 || tput AF 3 )
+blue=$(    tput setaf 4 || tput AF 4 )
+magenta=$( tput setaf 5 || tput AF 5 )
+cyan=$(    tput setaf 6 || tput AF 6 )
+white=$(   tput setaf 7 || tput AF 7 )
+
+# Background colors (use with unstyled colors)
+onblk=$(   tput setab 0 || tput AB 0 )
+onred=$(   tput setab 1 || tput AB 1 )
+ongrn=$(   tput setab 2 || tput AB 2 )
+onylw=$(   tput setab 3 || tput AB 3 )
+onblu=$(   tput setab 4 || tput AB 4 )
+onmag=$(   tput setab 5 || tput AB 5 )
+oncyn=$(   tput setab 6 || tput AB 6 )
+onwht=$(   tput setab 7 || tput AB 7 )
+
+# Normal colors
+txtblk=$reset$black
+txtred=$reset$red
+txtgrn=$reset$green
+txtylw=$reset$yellow
+txtblu=$reset$blue
+txtmag=$reset$magenta
+txtcyn=$reset$cyan
+txtwht=$reset$white
+
+# Bold colors
+bldblk=$bold$black
+bldred=$bold$red
+bldgrn=$bold$green
+bldylw=$bold$yellow
+bldblu=$bold$blue
+bldmag=$bold$magenta
+bldcyn=$bold$cyan
+bldwht=$bold$white
+
+# Underline colors
+undblk=$under$black
+undred=$under$red
+undgrn=$under$green
+undylw=$under$yellow
+undblu=$under$blue
+undmag=$under$magenta
+undcyn=$under$cyan
+undwht=$under$white
 
 status=$(git status --porcelain 2> /dev/null)
 
