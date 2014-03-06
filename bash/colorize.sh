8b3885cf6278056f054eea188afd80ba92c682b3
Justin J. Novack <jnovack@parxcasino.com>
2014-03-06 10:21:28 -0500

    learned the wonders of tput

--- bash/colorize.sh
+++ bash/colorize.sh
@@ -7,40 +7,61 @@
 # Purpose: Changes Root Prompt to be Red
 # Purpose: Add in graphic for non-zero exits
 # Documentation: Copy this file to /etc/profile.d/ and chmod +x
+# ChangeLog: Mar 06, 2014 - Learned the wonders of tput
 
-txtblk='\[\e[0;30m\]' # Black - Regular
-txtred='\[\e[0;31m\]' # Red
-txtgrn='\[\e[0;32m\]' # Green
-txtylw='\[\e[0;33m\]' # Yellow
-txtblu='\[\e[0;34m\]' # Blue
-txtpur='\[\e[0;35m\]' # Purple
-txtcyn='\[\e[0;36m\]' # Cyan
-txtwht='\[\e[0;37m\]' # White
-bldblk='\[\e[1;30m\]' # Black - Bold
-bldred='\[\e[1;31m\]' # Red
-bldgrn='\[\e[1;32m\]' # Green
-bldylw='\[\e[1;33m\]' # Yellow
-bldblu='\[\e[1;34m\]' # Blue
-bldpur='\[\e[1;35m\]' # Purple
-bldcyn='\[\e[1;36m\]' # Cyan
-bldwht='\[\e[1;37m\]' # White
-undblk='\[\e[4;30m\]' # Black - Underline
-undred='\[\e[4;31m\]' # Red
-undgrn='\[\e[4;32m\]' # Green
-undylw='\[\e[4;33m\]' # Yellow
-undblu='\[\e[4;34m\]' # Blue
-undpur='\[\e[4;35m\]' # Purple
-undcyn='\[\e[4;36m\]' # Cyan
-undwht='\[\e[4;37m\]' # White
-bakblk='\[\e[40m\]'   # Black - Background
-bakred='\[\e[41m\]'   # Red
-bakgrn='\[\e[42m\]'   # Green
-bakylw='\[\e[43m\]'   # Yellow
-bakblu='\[\e[44m\]'   # Blue
-bakpur='\[\e[45m\]'   # Purple
-bakcyn='\[\e[46m\]'   # Cyan
-bakwht='\[\e[47m\]'   # White
-txtrst='\[\e[0m\]'   	# Text Reset
+reset=$(   tput sgr0 )
+bold=$(    tput bold )
+under=$(   tput smul )
+
+# Unstyled colors
+black=$(   tput setaf 0 || tput AF 0 )
+red=$(     tput setaf 1 || tput AF 1 )
+green=$(   tput setaf 2 || tput AF 2 )
+yellow=$(  tput setaf 3 || tput AF 3 )
+blue=$(    tput setaf 4 || tput AF 4 )
+magenta=$( tput setaf 5 || tput AF 5 )
+cyan=$(    tput setaf 6 || tput AF 6 )
+white=$(   tput setaf 7 || tput AF 7 )
+
+# Background colors (use with unstyled colors)
+onblk=$(   tput setab 0 || tput AB 0 )
+onred=$(   tput setab 1 || tput AB 1 )
+ongrn=$(   tput setab 2 || tput AB 2 )
+onylw=$(   tput setab 3 || tput AB 3 )
+onblu=$(   tput setab 4 || tput AB 4 )
+onmag=$(   tput setab 5 || tput AB 5 )
+oncyn=$(   tput setab 6 || tput AB 6 )
+onwht=$(   tput setab 7 || tput AB 7 )
+
+# Normal colors
+txtblk=$reset$black
+txtred=$reset$red
+txtgrn=$reset$green
+txtylw=$reset$yellow
+txtblu=$reset$blue
+txtmag=$reset$magenta
+txtcyn=$reset$cyan
+txtwht=$reset$white
+
+# Bold colors
+bldblk=$bold$black
+bldred=$bold$red
+bldgrn=$bold$green
+bldylw=$bold$yellow
+bldblu=$bold$blue
+bldmag=$bold$magenta
+bldcyn=$bold$cyan
+bldwht=$bold$white
+
+# Underline colors
+undblk=$under$black
+undred=$under$red
+undgrn=$under$green
+undylw=$under$yellow
+undblu=$under$blue
+undmag=$under$magenta
+undcyn=$under$cyan
+undwht=$under$white
 
 # Set Prompt
 # \w = Full Path           eg.  /usr/www/html
@@ -53,7 +74,7 @@ txtrst='\[\e[0m\]'   	# Text Reset
 #   [Jan 01 12:34] [user@hostname ~]$
 
 if [ `id -u` = 0 ]; then
-	PS1="\`if [ \$? != 0 ]; then echo -e \"$bldred ಠ╭╮ಠ $txtred( return value:$bldred $? $txtred)\"; printf '\n'; fi; \`$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldred[$txtred\u@$bldred\h $txtgrn\w$bldred]$txtrst\\$ "
+	PS1="\`if [ \$? != 0 ]; then echo -e \"$bldred ಠ╭╮ಠ $txtred( return value:$bldred $? $txtred)\"; printf '\n'; fi; \`$txtwht[$txtmag\D{%b %d} $bldmag\A$txtwht] $bldred[$txtred\u@$bldred\h $txtgrn\w$bldred]$reset\\$ "
 else
-	PS1="\`if [ \$? != 0 ]; then echo -e \"$bldred ಠ╭╮ಠ $txtred( return value:$bldred $? $txtred)\"; printf '\n'; fi; \`$txtwht[$txtpur\D{%b %d} $bldpur\A$txtwht] $bldblu[$txtblu\u@$bldblu\h $txtgrn\w$bldblu]$txtrst\\$ "
+	PS1="\`if [ \$? != 0 ]; then echo -e \"$bldred ಠ╭╮ಠ $txtred( return value:$bldred $? $txtred)\"; printf '\n'; fi; \`$txtwht[$txtmag\D{%b %d} $bldmag\A$txtwht] $bldblu[$txtblu\u@$bldblu\h $txtgrn\w$bldblu]$reset\\$ "
 fi
