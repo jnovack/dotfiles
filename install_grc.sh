4f77a5d0ec7a327c40aa3ee2906ed779adb5f2ce
Justin J. Novack <jnovack@parxcasino.com>
2015-04-17 12:11:29 -0400

    update grace

--- install_grc.sh
+++ install_grc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp
-curl -O http://kassiopeia.juls.savba.sk/~garabik/software/grc/grc_1.5.tar.gz
-tar -xzvf grc_1.5.tar.gz
-cd grc-1.5
+curl -O http://kassiopeia.juls.savba.sk/~garabik/software/grc/grc_1.7.orig.tar.gz
+tar -xzvf grc_1.7.orig.tar.gz
+cd grc-1.7
 sudo ./install.sh
 sudo rm /usr/share/grc/conf.ping
 sudo ln -s $DIR/grc/conf.ping /usr/share/grc/conf.ping
