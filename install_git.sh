a5bbe16e1c546577a6d1e4a894e830f205ad3e94
Justin J. Novack <jnovack@gmail.com>
2017-02-23 21:28:02 -0500

    update git installer

--- install_git.sh
+++ install_git.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cat $DIR/_gitconfig >> ~/.gitconfig
+echo ".DS_Store" >> ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+echo
+echo ** You must run the following commands:
+echo      git config --global user.name "YOURNAME"
+echo      git config --global user.email "email@add.ress"
