8fb32cb096056ac71f4aa07e39cae6d7c010de83
Justin J. Novack <jnovack@users.noreply.github.com>
2019-12-11 07:38:46 -0500

    Update install_grc.sh

--- install_grc.sh
+++ install_grc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp
-curl -O http://kassiopeia.juls.savba.sk/~garabik/software/grc/grc_1.9.orig.tar.gz
-tar -xzvf grc_1.9.orig.tar.gz
-cd grc-1.9
+curl -LO https://github.com/garabik/grc/archive/v1.11.3.tar.gz
+tar -xzvf v1.11.3.tar.gz
+cd grc-1.11.3
 sudo ./install.sh
-sudo rm /usr/share/grc/conf.ping
-sudo ln -s $DIR/grc/conf.ping /usr/share/grc/conf.ping
+sudo rm /usr/local/share/grc/conf.ping
+sudo ln -s $DIR/grc/conf.ping /usr/local/share/grc/conf.ping
