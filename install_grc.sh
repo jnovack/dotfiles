6069030e0f5e45a26294ab9833e9bdd1cecdb44a
Justin J. Novack <jnovack@gmail.com>
2020-06-09 15:28:27 -0400

    more corrections... ugh

--- install_grc.sh
+++ install_grc.sh
@@ -2,7 +2,7 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd /tmp
 curl -LO https://github.com/garabik/grc/archive/v1.11.3.tar.gz
-tar -xzvf grc-1.11.3.tar.gz
+tar -xzvf v1.11.3.tar.gz
 cd grc-1.11.3
 sudo ./install.sh
 sudo rm /usr/local/share/grc/conf.ping
